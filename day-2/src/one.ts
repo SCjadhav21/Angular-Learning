158ecd8f4def3a2ce512af885804699319041ca9
Suraj Jadhav <jadhavsuraj4567@gmail.com>
2023-05-17 18:11:43 +0530

    adding angular demo

---
+++ day-2/src/one.ts
@@ -0,0 +1,9 @@
+let massage: string | number = "hello world";
+massage = 4;
+alert(massage);
+
+let data: Array<string> = ["data1", "data2", "data3", "data4"];
+
+alert(Math.floor(Math.random() * data.length - 1));
+
+let num: number[] = [1, 2, 3];
